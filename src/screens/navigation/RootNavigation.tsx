d40ce01258fb242fcda89632a6691b819cb7608c
Christophe <christophe@idruide.com>
2024-09-27 11:47:23 +0200

    feat: implement navigations screens

---
+++ src/screens/navigation/RootNavigation.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { TransitionPresets, createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigation from './AuthNavigation';
+import TabNavigation from './tabNavigation';
+
+const Stack = createStackNavigator();
+
+const RootNavigation = () => {
+  const [session, setSession] = useState(false);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.SlideFromRightIOS,
+          animationEnabled: true,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+        }}
+      >
+        {session ? (
+          <Stack.Screen name="TabNavigation" component={TabNavigation} />
+        ) : (
+          <Stack.Screen name="AuthNavigation" component={AuthNavigation} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
